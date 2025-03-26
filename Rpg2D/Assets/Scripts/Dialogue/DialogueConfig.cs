9a9a1a267c9d647e4a7f5096355a64012022017c
DanielSouzaCruz <danielrenovar165@gmail.com>
2025-03-26 16:20:52 -0300

    ScriptableObjects

---
+++ Rpg2D/Assets/Scripts/Dialogue/DialogueConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Talk", menuName = "New Talk/Talk")]
+public class DialogueConfig : ScriptableObject
+{
+    [Header("Config")]
+    public GameObject actor;
+
+    [Header("Talk")]
+    public Sprite speakerSprite;
+    public string sentence;
+
+    public List<Sentences> dialogues = new List<Sentences>();
+}
+
+[System.Serializable]
+public class Sentences
+{
+    public string actorName;
+    public Sprite profile;
+    public Languages sentence;
+}
+
+[System.Serializable]
+public class Languages
+{
+    public string english;
+    public string portuguese;
+    public string spanish;
+}
