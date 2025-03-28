8a10bdf61f687feb2cb89d76a240c7082f6f71eb
DanielSouzaCruz <danielrenovar165@gmail.com>
2025-03-27 22:39:10 -0300

    Dialogues

---
+++ Rpg2D/Assets/Scripts/Npc/NpcDialogue.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcDialogue : MonoBehaviour
+{
+    public float dialogueRange;
+    public LayerMask playerLayer;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        ShowDialogue();
+    }
+
+    void ShowDialogue()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, dialogueRange, playerLayer);
+        if(hit != null)
+        {
+
+        } else
+        {
+
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, dialogueRange);
+    }
+}
