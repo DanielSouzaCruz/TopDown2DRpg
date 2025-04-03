a17502bf0c5b7444d3171cbd5221f89d0df0c4d8
DanielSouzaCruz <danielrenovar165@gmail.com>
2025-04-03 16:08:43 -0300

    Props Colliders

---
+++ Rpg2D/Assets/Scripts/Tree.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Profiling;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    [SerializeField] private float life;
+    [SerializeField] private Animator anim;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnHit()
+    {
+        life--;
+        anim.SetTrigger("isHit");
+        if(life <= 0)
+        {
+            anim.SetTrigger("cut");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Axe"))
+        {
+            OnHit();
+        }
+    }
+}
