dd921eff1491ea57330c2a1cc1411668eb650fb9
DanielSouzaCruz <danielrenovar165@gmail.com>
2026-02-18 18:59:36 -0300

    Inimigo, inteligencia de movimentacao

---
+++ Rpg2D/Assets/Scripts/Enemy/AnimationControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControl : MonoBehaviour
+{
+    private Animator anim;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void PlayAnim(int value)
+    {
+        anim.SetInteger("transition", value);
+    }
+
+}
